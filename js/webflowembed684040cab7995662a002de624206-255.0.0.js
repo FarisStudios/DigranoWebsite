6e88e921360f4f65b92010a0514ca9835d5767b7
FarisStudios <64643065+FarisStudios@users.noreply.github.com>
2025-06-28 17:51:00 +0300

    Delete js directory

--- js/webflowembed684040cab7995662a002de624206-255.0.0.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('#mapsId').forEach((el) => el.remove());
-            var s=document.createElement('script');s.type='text/javascript'; s.async=true;
-            s.id = 'mapsId';
-            s.src='https://maps.bestfreecdn.com/storage/js/maps-4206.js?ver=255';
-            document.body.appendChild(s);
